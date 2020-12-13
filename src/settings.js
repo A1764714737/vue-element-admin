513eda97b9515bafb20515417f93f1304766686e
senboliang <1764714737@qq.com>
2020-12-13 21:11:39 +0800

    初始化项目#精简 login 模块#开启source-map devtool#修改网站标题

--- src/settings.js
+++ src/settings.js
@@ -1,27 +1,27 @@
 module.exports = {
-  title: 'Vue Element Admin',
+  title: '后台管理系统',
 
   /**
    * @type {boolean} true | false
-   * @description Whether show the settings right-panel
+   * @description 页面右侧的 可伸缩面板
    */
-  showSettings: true,
+  showSettings: false,
 
   /**
    * @type {boolean} true | false
-   * @description Whether need tagsView
+   * @description 页面标签 是否在 主页面中显示（侧边栏右侧的主页面）
    */
   tagsView: true,
 
   /**
    * @type {boolean} true | false
-   * @description Whether fix the header
+   * @description 头部 header 是否固定显示 （侧边栏右侧的主页面的上方）
    */
   fixedHeader: false,
 
   /**
    * @type {boolean} true | false
-   * @description Whether show the logo in sidebar
+   * @description 侧边栏顶部 logo 是否显示
    */
   sidebarLogo: false,
 
@@ -30,6 +30,7 @@ module.exports = {
    * @description Need show err logs component.
    * The default is only used in the production env
    * If you want to also use it in dev, you can pass ['production', 'development']
+   * 默认显示错误日志的环境
    */
   errorLog: 'production'
 }
